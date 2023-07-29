eb64a3ff3ac95a66d6fd26ff4dceb3ac9bbd6727
carlosleoncode <carlosestebanleonpinilla@gmail.com>
2023-07-28 19:41:41 -0500

    Updates

---
+++ precache-manifest.8a0fde60b9e82b4b40a79655ba4413c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0ff3305797aa0496b75",
+    "url": "/CarlosLeonCode/prctce_react_daily_tasks_app/static/css/main.08b8d2d3.chunk.css"
+  },
+  {
+    "revision": "c0ff3305797aa0496b75",
+    "url": "/CarlosLeonCode/prctce_react_daily_tasks_app/static/js/main.c0ff3305.chunk.js"
+  },
+  {
+    "revision": "78385fafb3b4f1628ccc",
+    "url": "/CarlosLeonCode/prctce_react_daily_tasks_app/static/js/1.78385faf.chunk.js"
+  },
+  {
+    "revision": "5f30aa6401e1d1d2d2e4",
+    "url": "/CarlosLeonCode/prctce_react_daily_tasks_app/static/js/runtime~main.5f30aa64.js"
+  },
+  {
+    "revision": "599ffe01d402b302516eaa0a025b2827",
+    "url": "/CarlosLeonCode/prctce_react_daily_tasks_app/index.html"
+  }
+];
