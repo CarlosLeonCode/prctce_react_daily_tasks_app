b5b6ffe7bcde9a2391bb4311e3e71c3694e818cc
carlosleoncode <carlosestebanleonpinilla@gmail.com>
2023-07-28 19:46:56 -0500

    Updates

---
+++ precache-manifest.a1bc2ef2c002bd5bfe9737a21137c237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0ff3305797aa0496b75",
+    "url": "/prctce_react_daily_tasks_app/static/css/main.08b8d2d3.chunk.css"
+  },
+  {
+    "revision": "c0ff3305797aa0496b75",
+    "url": "/prctce_react_daily_tasks_app/static/js/main.c0ff3305.chunk.js"
+  },
+  {
+    "revision": "78385fafb3b4f1628ccc",
+    "url": "/prctce_react_daily_tasks_app/static/js/1.78385faf.chunk.js"
+  },
+  {
+    "revision": "ff23522abd562b0157c6",
+    "url": "/prctce_react_daily_tasks_app/static/js/runtime~main.ff23522a.js"
+  },
+  {
+    "revision": "6808fee09eb2a4f6f344913e3e21dc8e",
+    "url": "/prctce_react_daily_tasks_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CarlosLeonCode/prctce_react_daily_tasks_app/precache-manifest.8a0fde60b9e82b4b40a79655ba4413c0.js"
+  "/prctce_react_daily_tasks_app/precache-manifest.a1bc2ef2c002bd5bfe9737a21137c237.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/CarlosLeonCode/prctce_react_daily_tasks_app/index.html", {
+workbox.routing.registerNavigationRoute("/prctce_react_daily_tasks_app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
